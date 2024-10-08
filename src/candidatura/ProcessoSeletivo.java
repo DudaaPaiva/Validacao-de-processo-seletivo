6c189c5baf9fcdedf6e8f626738dd5ad0b46d198
Eduarda Paiva <eduarda.a.paiva@gmail.com>
2024-10-08 12:04:17 -0300

    projeto de um processo seletivo

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,43 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    imprimirSelecionados();
+    String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO" };
+    for (String candidato : candidatos) {
+      entrandoEmContato(candidato);// chama o metodo
+
+    }
+  }
+
+  static void entrandoEmContato(String candidato) {// recebe como parametro a variavel candidato
+    int tentativasRealizadas = 1; // conta as tentativas de contato
+    boolean continuarTentando = true; // variavel que controla se continua tentando
+    boolean atendeu = false; // indica se candidato atendeu ou não
+    do {
+      atendeu = atender(); // chama a função que simula se o candidato atendeu ou não
+      continuarTentando = !atendeu; // se o candidato atender a variavel continuarTentando se torna false e para de
+                                    // tocar
+      if (continuarTentando)
+        tentativasRealizadas++;
+      else
+        System.out.println("Contato realizado com sucesso");
+
+    } while (continuarTentando && tentativasRealizadas < 3);
+
+    if (atendeu)
+      System.out.println("Conseguimos contato com " + candidato + " Na " + tentativasRealizadas + " tentativas.");
+
+    else
+      System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas "
+          + tentativasRealizadas + " realizadas ");
+  }
+
+  static boolean atender() {
+    return new Random().nextInt(3) == 1;// aqui determina um valor aleatorio entre 0 e 2 e quando for significa que o
+                                        // candidato atendeu
   }
 
   static void imprimirSelecionados() {
